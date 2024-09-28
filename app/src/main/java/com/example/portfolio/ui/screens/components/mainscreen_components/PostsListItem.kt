067602279860db716dcda64e32069d176ade8c0a
Alin <alin.dulceanu@gmail.com>
2024-09-28 13:40:10 +0300

    Refactored the Card Element for better reusability

--- app/src/main/java/com/example/portfolio/ui/screens/components/mainscreen_components/PostsListItem.kt
+++ app/src/main/java/com/example/portfolio/ui/screens/components/mainscreen_components/PostsListItem.kt
@@ -3,12 +3,9 @@ package com.example.portfolio.ui.screens.components.mainscreen_components
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.combinedClickable
-import androidx.compose.foundation.gestures.detectHorizontalDragGestures
 import androidx.compose.foundation.layout.Arrangement.Center
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults.cardColors
@@ -18,71 +15,48 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.text.style.TextOverflow.Companion.Ellipsis
 import androidx.compose.ui.tooling.preview.PreviewLightDark
-import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import com.example.portfolio.data.local.entities.PostsEntity
 import com.example.portfolio.ui.theme.PortfolioTheme
-import com.example.portfolio.viewmodels.events.Events
-import com.example.portfolio.viewmodels.events.Events.MainScreenEvents
-import com.example.portfolio.viewmodels.events.Events.MainScreenEvents.FavoritePost
-import com.example.portfolio.viewmodels.states.States
-import com.example.portfolio.viewmodels.states.States.MainState
-import kotlin.math.absoluteValue
-import kotlin.math.roundToInt
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
-fun PostsListItem(post: PostsEntity, onFavorite: () -> Unit, onDelete: () -> Unit) {
+fun PostsListItem(
+    post: PostsEntity,
+    modifier: Modifier = Modifier,
+    onLongClick: (() -> Unit)? = null,
+    onDoubleClick: (() -> Unit)? = null
+) {
+    val containerColor = if (post.isFavorited) {
+        colorScheme.primary
+    } else {
+        colorScheme.secondaryContainer
+    }
+
     var collapsed by remember {
         mutableStateOf(true)
     }
 
-    var dragX by remember {
-        mutableFloatStateOf(0f)
-    }
-
     val onClick: () -> Unit = { collapsed = !collapsed }
 
-    val containerColor = if (post.isFavorited) {
-        colorScheme.secondaryContainer
-    } else {
-        colorScheme.surfaceVariant
-    }
-
     Card(
-        modifier = Modifier
+        modifier = modifier
             .padding(vertical = 4.dp, horizontal = 16.dp)
-            .combinedClickable (
+            .combinedClickable(
                 onClick = onClick,
-                onLongClick = {},
-                onDoubleClick = onFavorite
-            )
-            .offset {
-                IntOffset(dragX.roundToInt(), 0)
-            }
-            .pointerInput(post.title) {
-                detectHorizontalDragGestures(
-                    onDragEnd = {
-                        if( dragX.absoluteValue > 500 )
-                            onDelete()
-                        dragX = 0f
-                    }
-                ) { _, dragAmount ->
-                    dragX += dragAmount
-                }
-            },
+                onLongClick = onLongClick,
+                onDoubleClick = onDoubleClick,
+            ),
         colors = cardColors(
             containerColor = containerColor
         )
-    ){
+    ) {
         Column(
             modifier = Modifier
                 .fillMaxWidth()
@@ -91,14 +65,14 @@ fun PostsListItem(post: PostsEntity, onFavorite: () -> Unit, onDelete: () -> Uni
         ) {
             Text(
                 text = post.title,
-                style = typography.titleLarge
+                style = typography.titleLarge,
+                modifier = Modifier.padding(bottom = 5.dp)
             )
 
             Text(
                 text = post.body,
                 style = typography.bodyLarge,
-                maxLines = if(collapsed) 2 else Int.MAX_VALUE,
-                color = colorScheme.onSurfaceVariant,
+                maxLines = if (collapsed) 2 else Int.MAX_VALUE,
                 overflow = Ellipsis,
             )
         }
@@ -107,7 +81,7 @@ fun PostsListItem(post: PostsEntity, onFavorite: () -> Unit, onDelete: () -> Uni
 
 @PreviewLightDark
 @Composable
-fun PreviewPostsListItem(){
+fun PreviewPostsListItem() {
     PortfolioTheme {
         Surface(
             modifier = Modifier
@@ -116,14 +90,17 @@ fun PreviewPostsListItem(){
         ) {
             Column {
                 PostsListItem(
-                    post = PostsEntity(id = 0, title = "Bla Bla", body = "Bla Bla Bla"),
-                    {},
-                    {})
+                    post = PostsEntity(id = 0, title = "Bla Bla", body = "Bla Bla Bla")
+                )
 
                 PostsListItem(
-                    post = PostsEntity(id = 0, title = "Bla Bla", body = "Bla Bla Bla", isFavorited = true),
-                    {},
-                    {})
+                    post = PostsEntity(
+                        id = 0,
+                        title = "Bla Bla",
+                        body = "Bla Bla Bla",
+                        isFavorited = true
+                    )
+                )
             }
         }
     }
