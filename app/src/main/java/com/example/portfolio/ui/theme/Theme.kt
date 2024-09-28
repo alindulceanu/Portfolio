cc301a1d0a87611439e0f934b12940181d218e10
Alin <alin.dulceanu@gmail.com>
2024-09-28 13:40:25 +0300

    Changed Status Bar color

--- app/src/main/java/com/example/portfolio/ui/theme/Theme.kt
+++ app/src/main/java/com/example/portfolio/ui/theme/Theme.kt
@@ -1,19 +1,15 @@
 package com.example.portfolio.ui.theme
 
 import android.app.Activity
-import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Immutable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
@@ -263,15 +259,15 @@ fun PortfolioTheme(
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
-      darkTheme -> darkScheme
-      else -> lightScheme
+        darkTheme -> darkScheme
+        else -> lightScheme
     }
 
     val view = LocalView.current
     if (!view.isInEditMode) {
         SideEffect {
             val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.secondaryContainer.toArgb()
+            window.statusBarColor = colorScheme.primaryContainer.toArgb()
             WindowCompat
                 .getInsetsController(window, view)
                 .isAppearanceLightStatusBars = darkTheme
@@ -279,8 +275,8 @@ fun PortfolioTheme(
     }
 
     MaterialTheme(
-      colorScheme = colorScheme,
-      typography = Typography,
-      content = content
+        colorScheme = colorScheme,
+        typography = Typography,
+        content = content
     )
 }
